40d122fc6fce61ff78b240fbd22d271f86c20858
Akshita05 <39550605+Akshita05@users.noreply.github.com>
2018-06-12 19:46:14 +0530

    Removed the errors

--- fd.py
+++ fd.py
@@ -9,7 +9,7 @@ def gui1():
     face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
     i=0
-    while(True & i<n) :
+    while(True and i<n) :
 
         check,frame=cap.read()
         gray=cv2.cvtColor(frame,cv2.COLOR_BGR2BGRA)
@@ -18,17 +18,17 @@ def gui1():
             frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),4)
         ri=cv2.resize(frame,(int(frame.shape[1]/3),int(frame.shape[0]/3)))
         cv2.imshow("1st",frame)
-        out = cv2.imwrite('capt'+str(i)+'.jpg', frame)
+        out = cv2.imwrite('captn'+str(i)+'.jpg', frame)
         if cv2.waitKey(1) & 0xFF== ord('q') :
             break
         i=i+1
-    video.release()
+    cap.release()
     cv2.destroyAllWindows()
-l1=tkinter.Label(window,"Enter the number of pictures you want to take :")
+l1=tkinter.Label(window,text='Enter the number of pictures you want to take :')
 l1.grid(row=0,column=0)
 b1=tkinter.Button(window,text="Enter",command=gui1)
-b1.grid(row=1,column=1)
-e1_val=StringVar()
+b1.grid(row=2,column=1)
+e1_val=tkinter.StringVar()
 e1=tkinter.Entry(window,textvariable=e1_val)
 e1.grid(row=1,column=1)
 
